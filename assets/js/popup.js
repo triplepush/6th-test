9bbe12d5f03c1d9817a5f0dc96176251a2ff6a09
LU,WEI-TZU <xjjoh099229@gmail.com>
2021-08-26 23:42:09 +0800

    add fade out

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -14,16 +14,17 @@ var video = document.getElementById("video_background");
     });
 */
  $(document).ready(function() {
-  $('body').css('display', 'none');
-  $('body').fadeIn(2000); //一開始淡入
-  $('a').click(function(event) { //點選 class 為 link 的 a 元件時觸發
-   event.preventDefault(); //取消事件預設動作
-   newLocation = this.href;
-   $('body').fadeOut(2000, newpage); //點選後淡出
-  });
+    $("body").fadeOut(2000);
+    $('body').css('display', 'none');
+    $('body').fadeIn(2000); //一開始淡入
+    $('a').click(function(event) { //點選 class 為 link 的 a 元件時觸發
+    event.preventDefault(); //取消事件預設動作
+    newLocation = this.href;
+    $('body').fadeOut(2000, newpage); //點選後淡出
+});
   
-  function newpage() {
-   window.location = newLocation;
-  }
- });
+    function newpage() {
+    window.location = newLocation;
+    }
+});
 
