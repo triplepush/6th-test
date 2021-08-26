1c6e008e352ebf258d9d686963c5d512d9aa0ad4
LU,WEI-TZU <xjjoh099229@gmail.com>
2021-08-27 03:24:15 +0800

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -20,7 +20,7 @@ var video = document.getElementById("video_background");
     $('a').click(function(event) { //點選 class 為 link 的 a 元件時觸發
     event.preventDefault(); //取消事件預設動作
     newLocation = this.href;
-    $('.wrapper').animate({
+    $('body').animate({
         width: '120%',
         height: '120%',
         opacity: 1,
