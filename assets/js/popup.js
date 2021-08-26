1913a295f36d5bee4801ad37250be02e339e9c88
LU,WEI-TZU <xjjoh099229@gmail.com>
2021-08-26 21:54:27 +0800

    mix img

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -3,7 +3,7 @@ $(function() {
         $(".loader").fadeOut(4000);
     });
 });
-
+/*
 var video = document.getElementById("video_background");
     video.addEventListener("canplay", function() {
     setTimeout(function() {
@@ -12,3 +12,18 @@ var video = document.getElementById("video_background");
         video.pause();
         
     });
+*/
+ $(document).ready(function() {
+  $('body').css('display', 'none');
+  $('body').fadeIn(2000); //一開始淡入
+  $('a').click(function(event) { //點選 class 為 link 的 a 元件時觸發
+   event.preventDefault(); //取消事件預設動作
+   newLocation = this.href;
+   $('body').fadeOut(2000, newpage); //點選後淡出
+  });
+  
+  function newpage() {
+   window.location = newLocation;
+  }
+ });
+
