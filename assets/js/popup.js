32ee7d11aac4370969d8152c054bd5b286355367
LU,WEI-TZU <xjjoh099229@gmail.com>
2021-08-27 03:20:49 +0800

    big mistake

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -16,20 +16,21 @@ var video = document.getElementById("video_background");
         
     });
 */
-/*
  $(document).ready(function() {
-    $('body').css('display', 'none');
-    $('body').fadeIn(1500); //一開始淡入
     $('a').click(function(event) { //點選 class 為 link 的 a 元件時觸發
     event.preventDefault(); //取消事件預設動作
     newLocation = this.href;
-    $('body').fadeOut(1500, newpage); //點選後淡出
+    $('.wrapper').animate({
+        width: '120%',
+        height: '120%',
+        opacity: 1,
+    }, 'normal' , newpage ); //點選後淡出
 });
     function newpage() {
     window.location = newLocation;
     }
 });
-*/
+
 function fadeInPage() {
     if (!window.AnimationEvent) { return; }
     var fader = document.getElementById('fader');
